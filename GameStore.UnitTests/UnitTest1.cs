bba1f9779677a5164caf612226ad4a4350225ade
felagunt <felagunt@live.ru>
2016-08-31 18:37:06 +0300

    colision version between test and webui need fix

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Moq;
-using GameStore.Domain.Abstract;
-using GameStore.Domain.Entities;
-using GameStore.WebUI.Controllers;
+using GameStore.WebUI.Models;
+using GameStore.WebUI.HtmlHelpers;
 
 namespace GameStore.UnitTests
 {
@@ -14,29 +13,37 @@ namespace GameStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
-            //arrange
-            Mock<IGameRepository> mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
+            //
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            //neccesary for using ext method
+            HtmlHelper myHelper = null;
+
+            //creating object PagingIngo
+            PageInfo pagingInfo = new PageInfo
             {
-                new Game { GameId = 1, Name = "Game1" },
-                new Game { GameId = 2, Name = "Game2" },
-                new Game { GameId = 3, Name = "Game3" },
-                new Game { GameId = 4, Name = "Game4" },
-                new Game { GameId = 5, Name = "Game5" }
-            });
-            GameController controller = new GameController(mock.Object);
-            controller.pageSize = 3;
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
 
             //act
-            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+            //wtf with versions
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            //
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                    + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                    + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                    result.ToString());
 
-            //assert
-            List<Game> game = result.ToList();
-            Assert.IsTrue(game.Count == 2);
-            Assert.AreEqual(game[0].Name, "Game4");
-            Assert.AreEqual(game[1].Name, "Game5");
+
         }
     }
 }
