e9bc621aef0f047e26e36ecb9d764760e34de7c9
felagunt <felagunt@live.ru>
2016-08-31 17:58:48 +0300

    add page routing : model and htmlHelper

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
 
 namespace GameStore.UnitTests
 {
@@ -9,6 +16,27 @@ namespace GameStore.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            //arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Game1" },
+                new Game { GameId = 2, Name = "Game2" },
+                new Game { GameId = 3, Name = "Game3" },
+                new Game { GameId = 4, Name = "Game4" },
+                new Game { GameId = 5, Name = "Game5" }
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            //act
+            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+
+            //assert
+            List<Game> game = result.ToList();
+            Assert.IsTrue(game.Count == 2);
+            Assert.AreEqual(game[0].Name, "Game4");
+            Assert.AreEqual(game[1].Name, "Game5");
         }
     }
 }
