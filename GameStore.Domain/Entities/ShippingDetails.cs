5ba4c0a4e09789387dc3ab0ea1d9c7ccb0bd7a03
felagunt <felagunt@live.ru>
2016-09-18 19:06:42 +0300

    order form slightly improve

--- GameStore.Domain/Entities/ShippingDetails.cs
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -13,14 +13,19 @@ namespace GameStore.Domain.Entities
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Enter your adress")]
+        [Display(Name="First addres")]
         public string Line1 { get; set; }
+        [Display(Name="Second addres")]
         public string Line2 { get; set; }
+        [Display(Name="Third")]
         public string Line3 { get; set; }
 
         [Required(ErrorMessage = "Select town")]
+        [Display(Name = "Town")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "Choose your country")]
+        [Display(Name = "Country")]
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
